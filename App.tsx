90d4be17055c5dec1efb755c6454337b61c55856
Pedroca12345 <pedrohenriqueanacleto789@gmail.com>
2026-07-08 22:51:18 -0300

    Evoluindo na aplicação mobile

--- App.tsx
+++ App.tsx
@@ -1,10 +1,13 @@
 import { Navigation } from "./src/routes/stack/index"
 import { UserContextProvider } from './src/providers/UserContextProvider';
+import { PartContextProvider } from "./src/providers/PartContextProvider/PartContextProvider";
 
 export default function App() {
   return (
     <UserContextProvider>
-      <Navigation />
+      <PartContextProvider>
+        <Navigation />
+      </PartContextProvider>
     </UserContextProvider>
   );
 }
